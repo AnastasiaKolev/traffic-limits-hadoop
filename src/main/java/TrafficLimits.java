fb8f4edd98ade8a7862886ed4949f4a4c46bd17a
Anastasia <anastasia.kolevatykh@gmail.com>
2019-10-24 02:52:16 +0300

    Added spark context, hive, kafka examples

--- src/main/java/TrafficLimits.java
+++ src/main/java/TrafficLimits.java
@@ -1,21 +1,123 @@
-import org.pcap4j.core.*;
-import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
-import org.pcap4j.packet.IpV4Packet;
-import org.pcap4j.packet.Packet;
-import org.pcap4j.util.NifSelector;
-import org.pcap4j.util.Packets;
 
+import groovy.lang.Tuple2;
+import org.apache.hadoop.hive.llap.Row;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.StorageLevels;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.StreamingContext;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.pcap4j.core.*;
+import org.pcap4j.util.NifSelector;
+
+import java.io.File;
 import java.io.IOException;
-import java.io.EOFException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.concurrent.TimeoutException;
+import java.util.Arrays;
 
 public class TrafficLimits {
+    protected static JavaSparkContext sc;
+    protected static SparkConf sparkConf;
 
-    private static int totalPacketLength = 0;
+    private static int totalPacketLength;
+    private static int maxPacketLength = 1073741824;    // todo: get from hive
+    private static int minPacketLength = 1024;          // todo: get from hive
 
+
+    public static void main(String[] args) throws PcapNativeException, NotOpenException {
+        setup( args );
+        execute();
+    }
+
+    public static void setup(String[] args) {
+        sparkConf = new SparkConf().setAppName( "Traffic_Limits" );
+        sc = new JavaSparkContext( sparkConf );
+    }
+
+    public static void execute() throws PcapNativeException, NotOpenException {
+
+        // Выбираем узел сети
+        PcapNetworkInterface device = getNetworkDevice();
+        System.out.println( "You chose: " + device );
+
+        // Проверка существования узла
+        if (device == null) {
+            System.out.println( "No device chosen." );
+            System.exit( 1 );
+        }
+
+        // Открываем узел и запускаем управляющий модуль handle
+        int snapshotLength = 65536; // in bytes
+        int readTimeout = 500;      // in milliseconds
+        final PcapHandle handle;
+        handle = device.openLive( snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout );
+
+        final long[] startTime = {System.currentTimeMillis()};
+
+        // Create the context with a 5 minutes batch size
+//        JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.minutes( 5 ));
+//        JavaReceiverInputDStream<String> packets = ssc.socketStream( handle.getNextPacketEx() );
+
+//        JavaReceiverInputDStream<String> lines = ssc.socketTextStream(
+//                        args[0], Integer.parseInt(args[1]), StorageLevels.MEMORY_AND_DISK_SER);
+//        JavaDStream<String> words = lines.flatMap( x -> Arrays.asList(SPACE.split(x)).iterator());
+//        JavaPairDStream<String, Integer> wordCounts = words.mapToPair( s -> new Tuple2<>(s, 1))
+//                        .reduceByKey((i1, i2) -> i1 + i2);
+//
+//        wordCounts.print();
+//        ssc.start();
+//        ssc.awaitTermination();
+
+        PacketListener listener = new PacketListener() {
+            @Override
+            public void gotPacket(PcapPacket pcapPacket) {
+                int len = pcapPacket.length();
+                totalPacketLength += len;
+                
+                // захват трафика за 5 минут
+                if (startTime[0] > 5 * 60 * 1000) {
+                    if (totalPacketLength < minPacketLength) {
+                        // send alert to Kafka
+                        sendAlert();
+
+                        System.out.println( "total.length() is less than min value " + totalPacketLength );
+                    }
+                    if (totalPacketLength > maxPacketLength) {
+                        // send alert to Kafka
+                        sendAlert();
+
+                        System.out.println( "total.length() is higher than max value " + totalPacketLength );
+                    }
+                    totalPacketLength = 0;
+                    startTime[0] = System.currentTimeMillis();
+                }
+
+                System.out.println( "1.pcapPacket.length() " + len );
+                System.out.println( "2.handle.getTimestampPrecision() " + handle.getTimestampPrecision() );
+                System.out.println( "3.pcapPacket " + pcapPacket );
+            }
+        };
+
+        try {
+            int maxPackets = 0;     // 0 = INFINITE
+            handle.loop( maxPackets, listener );
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Cleanup when complete
+        handle.close();
+
+        System.out.println( "Total Packet Length " + totalPacketLength
+                + " per " + readTimeout + " milliseconds" );
+    }
+
+    // todo: select network interface by args
     private static PcapNetworkInterface getNetworkDevice() {
         PcapNetworkInterface device = null;
         try {
@@ -26,83 +128,46 @@ public class TrafficLimits {
         return device;
     }
 
-    public static void main(String[] args) throws PcapNativeException, NotOpenException {
-        // The code we had before
-        PcapNetworkInterface device = getNetworkDevice();
-        System.out.println( "You chose: " + device );
-
-        // New code below here
-        if (device == null) {
-            System.out.println( "No device chosen." );
-            System.exit( 1 );
-        }
-
-        // Open the device and get a handle
-        int snapshotLength = 65536; // in bytes
-        int readTimeout = 500; // in milliseconds
-        final PcapHandle handle;
-        handle = device.openLive( snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout );
-
-
-//        int max=1073741824;
-//        int sum = 0;
+    // todo: send alert to Kafka (example)
+    public static void sendAlert() {
+//        DataFrame<Row> df = spark
+//                .write()
+//                .format( "kafka" )
+//                .option( "kafka.bootstrap.servers", "host1:port1,host2:port2" );
 //
-//        // current = текущее время
+//        StreamingQuery ds = df
+//                .selectExpr( "CAST(key AS STRING)", "CAST(value AS STRING)" )
+//                .writeStream()
+//                .format( "kafka" )
+//                .option( "kafka.bootstrap.servers", "host1:port1,host2:port2" )
+//                .option( "topic", "alerts" )
+//                .start();
+    }
 
-        PacketListener listener = new PacketListener() {
-            @Override
-            public void gotPacket(PcapPacket pcapPacket) {
-                int len = pcapPacket.length();
 
-//                test();
-//                t++;
-//
-//                // newCurrent = текущее время
-//
-//               /* if (newCurrent - current >= 5*//*минут*//*) {
-//                    if (sum > max) {
-//                        //alert
-//                        sum = 0;
-//                    } else {
-//                        sum = 0;
-//                    }
-//                }*/
-//
-                totalPacketLength += len;
-                System.out.println( "1.pcapPacket.length() "+ len );
-                System.out.println( "2.handle.getTimestampPrecision() " + handle.getTimestampPrecision() );
-                System.out.println( "3.pcapPacket "  + pcapPacket );
-            }
-        };
+    // todo: example for read limits from Hive
+    private static void readLimits() {
+        String warehouseLocation = new File("spark-warehouse").getAbsolutePath();
+        SparkSession spark = SparkSession
+                .builder()
+                .appName( "Java Spark Hive Example" )
+                .config( "spark.sql.warehouse.dir", warehouseLocation )
+                .enableHiveSupport()
+                .getOrCreate();
 
-        try {
-            int maxPackets = 50;
-            handle.loop( maxPackets, listener );
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        spark.sql( "SELECT * FROM limits_per_hour" );
+    }
 
-        // Cleanup when complete
-        handle.close();
+    // todo: example for write limits to Hive
+    private static void writeLimits() {
+        String warehouseLocation = new File("spark-warehouse").getAbsolutePath();
+        SparkSession spark = SparkSession
+                .builder()
+                .appName( "Java Spark Hive Example" )
+                .config( "spark.sql.warehouse.dir", warehouseLocation )
+                .enableHiveSupport()
+                .getOrCreate();
 
-//
-//        InetAddress addr = InetAddress.getByName("192.168.10.100");
-//        PcapNetworkInterface nif = Pcaps.getDevByAddress(addr);
-//
-//        int snapLen = 65536;
-//        PcapNetworkInterface.PromiscuousMode mode = PcapNetworkInterface.PromiscuousMode.PROMISCUOUS;
-//        int timeout = 10;
-//        PcapHandle handle = nif.openLive(snapLen, mode, timeout);
-//
-//        Packet packet = handle.getNextPacketEx();
-//        handle.close();
-
-        System.out.println( "Total Packet Length " + totalPacketLength + " per " + readTimeout + " milliseconds");
-//
-
-//        IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
-//        System.out.println( "4.packet.get(IpV4Packet.class) " + ipV4Packet );
-//        Inet4Address srcAddr = ipV4Packet.getHeader().getSrcAddr();
-//        System.out.println( "5.ipV4Packet.getHeader().getSrcAddr() " + srcAddr);
+        spark.sql( "INSERT INTO TABLE limits_per_hour VALUES ..." );
     }
 }
